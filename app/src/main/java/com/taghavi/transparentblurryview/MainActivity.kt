0b08c26900d26926292ba39a5ed6ab5e7fd820f5
EhsanTgv <EhsanTgv@gmail.com>
2021-02-11 14:21:34 +0330

    Implement Simple Blur Image

--- app/src/main/java/com/taghavi/transparentblurryview/MainActivity.kt
+++ app/src/main/java/com/taghavi/transparentblurryview/MainActivity.kt
@@ -2,10 +2,15 @@ package com.taghavi.transparentblurryview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val blurDrawable = BlurDrawable(beneathView, 50)
+
+        blurView.background = blurDrawable
     }
 }
